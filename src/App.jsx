8b84fe4b3459fc45086eb7a8a0be746200db1849
zeeshanalikhan15 <zeeshana@virtualq.com>
2023-11-12 00:15:58 +0500

    add taiwind

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,10 @@ function App() {
         <Technologies/>
         <Education/>
         <ContactInfo />
+
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </div>
     </>
   )
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -2,15 +2,27 @@ import './Navigation.css'
 
 function Navigation() {
   return (
-    <nav>
-      <ul>
-        <li><a href="#work-experience">Work Experience</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#technologies">Technologies</a></li>
-        <li><a href="#education">Education</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-    </nav>
+    <div>
+      <nav className="hidden">
+        <ul>
+          <li><a href="#work-experience">Work Experience</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#technologies">Technologies</a></li>
+          <li><a href="#education">Education</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+
+      <nav className="md:hidden">
+        <ul>
+          <li><a href="#work-experience">Work Experience</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#technologies">Technologies</a></li>
+          <li><a href="#education">Education</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
