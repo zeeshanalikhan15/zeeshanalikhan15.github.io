d8ce1806b23ceb88b90a93f5d58b01be251f7034
zeeshanalikhan15 <zeeshana@virtualq.com>
2023-11-13 00:23:12 +0500

    improve header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,11 @@
-import './Header.css'
+
 
 function Header() {
   return (
-    <header id="main-header">
-      <h1>Zeeshan Ali Khan</h1>
-      <p>Software Engineer</p>
-      <p>VoIP & Contact Center Solution Specialist: Aligning Advanced Communication Systems with Business Needs</p>
+    <header id="main-header" className="bg-black text-white">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl mb-4">Zeeshan Ali Khan</h1>
+      <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl mb-3">Software Engineer</p>
+      <p className="break-normal text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl ml-3 mr-3">VoIP & Contact Center Solution Specialist: Aligning Advanced Communication Systems with Business Needs</p>
     </header>
   );
 }
