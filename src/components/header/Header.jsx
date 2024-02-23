50a8506c633efc9f2dd1f1cb54600d231b29ded4
zeeshanalikhan15 <zeeshana@virtualq.com>
2024-02-23 16:11:27 +0500

    add new nav bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,10 +2,10 @@
 
 function Header() {
   return (
-    <header id="main-header" className="bg-black text-white">
+    <header id="main-header" className="bg-gray-800 text-white">
       <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl mb-4">Zeeshan Ali Khan</h1>
       <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl mb-3">Software Engineer</p>
-      <p className="break-normal text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl ml-3 mr-3">VoIP & Contact Center Solution Specialist: Aligning Advanced Communication Systems with Business Needs</p>
+      <p className="break-normal text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl ml-3 mr-3 text-gray-300">VoIP & Contact Center Solution Specialist: Aligning Advanced Communication Systems with Business Needs</p>
     </header>
   );
 }
