bcfd58b004b2b8929f4c449039923122dea6f92d
zeeshanalikhan15 <zeeshana@virtualq.com>
2024-02-23 16:21:48 +0500

    add taiwind

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
  
 
   return ( 
-    <div>
+    <div className="flex-1 items-center justify-center sm:items-stretch sm:justify-start">
       <Header />
       <Navigation />
       <WorkExperience/>
