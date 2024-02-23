36289cabf53d0578fae47db89cc8615b06c09f0a
zeeshanalikhan15 <zeeshana@virtualq.com>
2024-02-23 18:22:04 +0500

    aa bit more space in contact info

--- src/components/contact/ContactInfo.jsx
+++ src/components/contact/ContactInfo.jsx
@@ -7,6 +7,8 @@ function ContactInfo() {
           title="Contact"
           content= {          
             <div className="contact-info">
+              <br/>
+              <br/>
               <p>Email: ZeeshanAliKhan15@gmail.com</p>
               <p>LinkedIn: <a href="https://www.linkedin.com/in/zeeshanalikhan15/">https://www.linkedin.com/in/zeeshanalikhan15/</a></p>
               <p>GitHub: <a href="https://github.com/zeeshanalikhan15">https://github.com/zeeshanalikhan15</a></p>
