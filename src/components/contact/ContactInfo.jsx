fe3290de0123339f1def6090748d1891fae8bbf7
zeeshanalikhan15 <zeeshana@virtualq.com>
2024-02-23 18:14:38 +0500

    improve color mode support

--- src/components/contact/ContactInfo.jsx
+++ src/components/contact/ContactInfo.jsx
@@ -1,22 +1,17 @@
 import './ContactInfo.css'
-import upIcon  from '../../assets/up-arrow.png'
+import Section from '../section/Section'
 
 function ContactInfo() {
-  return (
-    <section id="contact">
-      <div className="contact-info">
-        <h2>
-          Contact
-          <a href="#main-header">
-            <img style={{ width: '40px', height: '40px' }} src={upIcon} alt="Scroll to top" />
-          </a>
-        </h2>
-        <p>Email: ZeeshanAliKhan15@gmail.com</p>
-        <p>LinkedIn: <a href="https://www.linkedin.com/in/zeeshanalikhan15/">https://www.linkedin.com/in/zeeshanalikhan15/</a></p>
-        <p>GitHub: <a href="https://github.com/zeeshanalikhan15">https://github.com/zeeshanalikhan15</a></p>
-      </div>
-    </section>
-  );
+  return (<Section
+          id="contact"
+          title="Contact"
+          content= {          
+            <div className="contact-info">
+              <p>Email: ZeeshanAliKhan15@gmail.com</p>
+              <p>LinkedIn: <a href="https://www.linkedin.com/in/zeeshanalikhan15/">https://www.linkedin.com/in/zeeshanalikhan15/</a></p>
+              <p>GitHub: <a href="https://github.com/zeeshanalikhan15">https://github.com/zeeshanalikhan15</a></p>
+          </div>}
+          />
+      );
 }
-
 export default ContactInfo;
