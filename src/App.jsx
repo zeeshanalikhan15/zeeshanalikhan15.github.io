0cdb236f83be1079d42f7a8cdf788cc756da7ab6
zeeshanalikhan15 <zeeshanalikhan15@gmail.com>
2024-12-28 00:11:55 +0100

    fixed header and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import Header from './components/header/Header'
-import Navigation from './components/navigation/Navigation'
 import ContactInfo from './components/contact/ContactInfo'
 import WorkExperience from './components/work/WorkExperience'
 import Projects from './components/projects/Projects'
@@ -8,16 +7,13 @@ import Technologies from './components/tech/Technologies'
 import Education from './components/education/Education'
 
 function App() {
- 
-
-  return ( 
+  return (
     <div className="flex-1 items-center justify-center sm:items-stretch sm:justify-start">
       <Header />
-      <Navigation />
-      <WorkExperience/>
-      <Projects/>
-      <Technologies/>
-      <Education/>
+      <WorkExperience />
+      <Projects />
+      <Technologies />
+      <Education />
       <ContactInfo />
     </div>
   )
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -16,15 +16,14 @@ function classNames(...classes) {
 
 export default function Navigation() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-gray-800 w-full">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
+            <div className="relative flex h-16 items-center justify-center">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="absolute -inset-0.5" />
+                {/* Mobile menu button */}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -33,8 +32,8 @@ export default function Navigation() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="hidden sm:ml-6 sm:block">
+              <div className="flex flex-1 items-center justify-center">
+                <div className="hidden sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <a
