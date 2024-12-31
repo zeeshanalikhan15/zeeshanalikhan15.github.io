0f153968f0aebc7e584c5a2a346c377c788e967f
zeeshanalikhan15 <zeeshanalikhan15@gmail.com>
2024-12-31 02:03:08 +0100

    divide overivew

---
+++ src/components/overview/Graphs.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import MobileGraphs from './MobileGraphs';
+import DesktopGraphs from './DesktopGraphs';
+
+const Graphs = () => {
+    return (
+        <div className="relative bg-white p-8 rounded-md shadow-md text-left">
+            <h3 className="text-2xl font-bold mb-4">Technologies</h3>
+            <p className="text-base mb-4">Throughout my career, I have worked with a variety of technologies including Avaya Elite, Avaya CIE, Avaya Aura Contact Center (AACC), Voxtron, Twilio, Asterisk, TCP/IP Network Programming, SI code, ACD implementations, ININ, and various network switches.</p>
+            <div className="block lg:hidden">
+                <MobileGraphs />
+            </div>
+            <div className="hidden lg:block">
+                <DesktopGraphs />
+            </div>
+        </div>
+    );
+};
+
+export default Graphs;
--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
-import { workExperienceData } from '../../data/data';
-import OverviewExperienceItem from './OverviewExperienceItem';
-import MobileGraphs from './MobileGraphs';
-import DesktopGraphs from './DesktopGraphs';
+import Experience from './Experience';
+import Graphs from './Graphs';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
@@ -12,31 +10,8 @@ const Overview = () => {
         <section id="overview" className="my-8 p-6 bg-gray-200 shadow-md rounded-lg">
             <h2 className="text-4xl font-bold mb-6 bg-gray-200 p-2 rounded-md text-center">Overview</h2>
             <div className="space-y-10 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-10">
-                <div className="relative bg-white p-8 rounded-md shadow-md text-left">
-                    <h3 className="text-2xl font-bold mb-4">Work Experience</h3>
-                    <ul className="list-none p-0 relative">
-                        <div className="absolute left-4 top-0 w-1 bg-gray-300 h-full"></div>
-                        {workExperienceData.map((experience, index) => (
-                            <OverviewExperienceItem
-                                key={index}
-                                date={experience.date}
-                                title={experience.title}
-                                company={experience.company}
-                                location={experience.location}
-                            />
-                        ))}
-                    </ul>
-                </div>
-                <div className="relative bg-white p-8 rounded-md shadow-md text-left">
-                    <h3 className="text-2xl font-bold mb-4">Technologies</h3>
-                    <p className="text-base mb-4">Throughout my career, I have worked with a variety of technologies including Avaya Elite, Avaya CIE, Avaya Aura Contact Center (AACC), Voxtron, Twilio, Asterisk, TCP/IP Network Programming, SI code, ACD implementations, ININ, and various network switches.</p>
-                    <div className="block lg:hidden">
-                        <MobileGraphs />
-                    </div>
-                    <div className="hidden lg:block">
-                        <DesktopGraphs />
-                    </div>
-                </div>
+                <Experience />
+                <Graphs />
             </div>
         </section>
     );
