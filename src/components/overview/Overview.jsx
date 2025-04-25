7685b3e982fe8ebc317e1b9f60654a91de4a34ea
zeeshanalikhan15 <zeeshanalikhan15@gmail.com>
2025-04-25 21:17:10 +0200

    remove unwanted expreinces componenet

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
-import MobileExperience from './MobileExperience';
-import Experience from './Experience';
+import OverviewExperience from './OverviewExperience';
 import Graphs from './Graphs';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const Overview = () => {
     return (
-        <section id="overview" className="my-8 p-6 bg-gray-200 shadow-md rounded-lg">
-            <h2 className="text-4xl font-bold mb-6 bg-gray-200 p-2 rounded-md text-center">Overview</h2>
-            <div className="space-y-10 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-10">
-                <div className="block lg:hidden">
-                    <MobileExperience />
-                </div>
-                <div className="hidden lg:block">
-                    <Experience />
+        <section id="overview" className="my-12 p-8 bg-white shadow-lg rounded-lg">
+            <h2 className="text-4xl font-bold text-center">Overview</h2>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+                <div className="animate-fade-in">
+                    <OverviewExperience />
                 </div>
                 <Graphs />
             </div>
